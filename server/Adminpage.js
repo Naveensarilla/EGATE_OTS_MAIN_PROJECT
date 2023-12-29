34e9926e09574cf88371aa193e7658a8e5dd98c9
sindhu-121 <sindhu.g@egradtutor.in>
2023-12-29 10:53:18 +0530

    hhjghjghj

--- server/Adminpage.js
+++ server/Adminpage.js
@@ -1867,241 +1867,241 @@ app.get('/sections/:subjectId/:testCreationTableId', async (req, res) => {
 
 
 // doc upload code -----------------
-// app.post("/upload", upload.single("document"), async (req, res) => {
-//   const docxFilePath = `uploads/${req.file.filename}`;
-//   const outputDir = `uploads/${req.file.originalname}_images`;
-
-//   const docName = `${req.file.originalname}`;
-//   try {
-//     await fs.mkdir(outputDir, { recursive: true });
-//     const result = await mammoth.convertToHtml({ path: docxFilePath });
-//     const htmlContent = result.value;
-//     const $ = cheerio.load(htmlContent);
-//     const textResult = await mammoth.extractRawText({ path: docxFilePath });
-//     const textContent = textResult.value;
-//     const textSections = textContent.split("\n\n");
-
-//     // Insert documentName and get documentId
-//     const [documentResult] = await db.query("INSERT INTO ots_document SET ?", {
-//       documen_name: docName,
-//       testCreationTableId: req.body.testCreationTableId,
-//       subjectId: req.body.subjectId,
-//     });
-//     const document_Id = documentResult.insertId;
-
-//     // Get all images in the order they appear in the HTML
-//     const images = [];
-//     $("img").each(function (i, element) {
-//       const base64Data = $(this)
-//         .attr("src")
-//         .replace(/^data:image\/\w+;base64,/, "");
-//       const imageBuffer = Buffer.from(base64Data, "base64");
-//       images.push(imageBuffer);
-//     });
-
-//     let j = 0;
-//     let Question_id;
-//     for (let i = 0; i < images.length; i++) {
-//       if (j == 0) {
-//         const questionRecord = {
-//           question_img: images[i],
-//           testCreationTableId: req.body.testCreationTableId,
-//           sectionId: req.body.sectionId,
-//           document_Id: document_Id,
-//           subjectId: req.body.subjectId,
-//         };
-//         console.log(j);
-//         Question_id = await insertRecord("questions", questionRecord);
-//         j++;
-//       } else if (j > 0 && j < 5) {
-//         const optionRecord = {
-//           option_img: images[i],
-//           question_id: Question_id,
-//         };
-//         console.log(j);
-//         await insertRecord("options", optionRecord);
-//         j++;
-//       } else if (j == 5) {
-//         const solutionRecord = {
-//           solution_img: images[i],
-//           question_id: Question_id,
-//         };
-//         console.log(j);
-//         await insertRecord("solution", solutionRecord);
-//         j = 0;
-//       }
-//     }
-//     res.send(
-//       "Text content and images extracted and saved to the database with the selected topic ID successfully."
-//     );
-//   } catch (error) {
-//     console.error(error);
-//     res
-//       .status(500)
-//       .send("Error extracting content and saving it to the database.");
-//   }
-// });
-
 app.post("/upload", upload.single("document"), async (req, res) => {
   const docxFilePath = `uploads/${req.file.filename}`;
   const outputDir = `uploads/${req.file.originalname}_images`;
 
   const docName = `${req.file.originalname}`;
   try {
-    // Check if a document with the same name already exists
-    const [existingDoc] = await db.query(
-      "SELECT document_Id FROM ots_document WHERE documen_name = ?",
-      [docName]
-    );
-  
-    if (existingDoc.length > 0) {
-      return res.status(409).send("Document with the same name already exists.");
-    }
-  
-    let existingTestSubjectDoc;
-  
-    // Check if a section is specified
-    if (req.body.sectionId) {
-      // Check if a document with the same test, subject, and section already exists
-      [existingTestSubjectDoc] = await db.query(
-        "SELECT document_Id FROM ots_document WHERE testCreationTableId = ? AND subjectId = ? AND sectionId = ?",
-        [req.body.testCreationTableId, req.body.subjectId, req.body.sectionId]
-      );
-    } else {
-      // Check if a document with the same test and subject already exists
-      [existingTestSubjectDoc] = await db.query(
-        "SELECT document_Id FROM ots_document WHERE testCreationTableId = ? AND subjectId = ? AND sectionId IS NULL",
-        [req.body.testCreationTableId, req.body.subjectId]
-      );
-    }
-  
-    if (existingTestSubjectDoc.length > 0) {
-      return res.status(409).send("Document with the same test, subject, and section already exists.");
-    }
     await fs.mkdir(outputDir, { recursive: true });
-        const result = await mammoth.convertToHtml({ path: docxFilePath });
-        const htmlContent = result.value;
-        const $ = cheerio.load(htmlContent);
-        const textResult = await mammoth.extractRawText({ path: docxFilePath });
-        const textContent = textResult.value;
-        const textSections = textContent.split("\n\n");
-    
-        // Insert documentName and get documentId
-        const [documentResult] = await db.query("INSERT INTO ots_document SET ?", {
-          documen_name: docName,
+    const result = await mammoth.convertToHtml({ path: docxFilePath });
+    const htmlContent = result.value;
+    const $ = cheerio.load(htmlContent);
+    const textResult = await mammoth.extractRawText({ path: docxFilePath });
+    const textContent = textResult.value;
+    const textSections = textContent.split("\n\n");
+
+    // Insert documentName and get documentId
+    const [documentResult] = await db.query("INSERT INTO ots_document SET ?", {
+      documen_name: docName,
+      testCreationTableId: req.body.testCreationTableId,
+      subjectId: req.body.subjectId,
+    });
+    const document_Id = documentResult.insertId;
+
+    // Get all images in the order they appear in the HTML
+    const images = [];
+    $("img").each(function (i, element) {
+      const base64Data = $(this)
+        .attr("src")
+        .replace(/^data:image\/\w+;base64,/, "");
+      const imageBuffer = Buffer.from(base64Data, "base64");
+      images.push(imageBuffer);
+    });
+
+    let j = 0;
+    let Question_id;
+    for (let i = 0; i < images.length; i++) {
+      if (j == 0) {
+        const questionRecord = {
+          question_img: images[i],
           testCreationTableId: req.body.testCreationTableId,
+          sectionId: req.body.sectionId,
+          document_Id: document_Id,
           subjectId: req.body.subjectId,
-          sectionId:req.body.sectionId
-        });
-        const document_Id = documentResult.insertId;
+        };
+        console.log(j);
+        Question_id = await insertRecord("questions", questionRecord);
+        j++;
+      } else if (j > 0 && j < 5) {
+        const optionRecord = {
+          option_img: images[i],
+          question_id: Question_id,
+        };
+        console.log(j);
+        await insertRecord("options", optionRecord);
+        j++;
+      } else if (j == 5) {
+        const solutionRecord = {
+          solution_img: images[i],
+          question_id: Question_id,
+        };
+        console.log(j);
+        await insertRecord("solution", solutionRecord);
+        j = 0;
+      }
+    }
+    res.send(
+      "Text content and images extracted and saved to the database with the selected topic ID successfully."
+    );
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("Error extracting content and saving it to the database.");
+  }
+});
+
+// app.post("/upload", upload.single("document"), async (req, res) => {
+//   const docxFilePath = `uploads/${req.file.filename}`;
+//   const outputDir = `uploads/${req.file.originalname}_images`;
+
+//   const docName = `${req.file.originalname}`;
+//   try {
+//     // Check if a document with the same name already exists
+//     const [existingDoc] = await db.query(
+//       "SELECT document_Id FROM ots_document WHERE documen_name = ?",
+//       [docName]
+//     );
+  
+//     if (existingDoc.length > 0) {
+//       return res.status(409).send("Document with the same name already exists.");
+//     }
+  
+//     let existingTestSubjectDoc;
+  
+//     // Check if a section is specified
+//     if (req.body.sectionId) {
+//       // Check if a document with the same test, subject, and section already exists
+//       [existingTestSubjectDoc] = await db.query(
+//         "SELECT document_Id FROM ots_document WHERE testCreationTableId = ? AND subjectId = ? AND sectionId = ?",
+//         [req.body.testCreationTableId, req.body.subjectId, req.body.sectionId]
+//       );
+//     } else {
+//       // Check if a document with the same test and subject already exists
+//       [existingTestSubjectDoc] = await db.query(
+//         "SELECT document_Id FROM ots_document WHERE testCreationTableId = ? AND subjectId = ? AND sectionId IS NULL",
+//         [req.body.testCreationTableId, req.body.subjectId]
+//       );
+//     }
+  
+//     if (existingTestSubjectDoc.length > 0) {
+//       return res.status(409).send("Document with the same test, subject, and section already exists.");
+//     }
+//     await fs.mkdir(outputDir, { recursive: true });
+//         const result = await mammoth.convertToHtml({ path: docxFilePath });
+//         const htmlContent = result.value;
+//         const $ = cheerio.load(htmlContent);
+//         const textResult = await mammoth.extractRawText({ path: docxFilePath });
+//         const textContent = textResult.value;
+//         const textSections = textContent.split("\n\n");
     
-        // Get all images in the order they appear in the HTML
-        const images = [];
-        $("img").each(function (i, element) {
-          const base64Data = $(this)
-            .attr("src")
-            .replace(/^data:image\/\w+;base64,/, "");
-          const imageBuffer = Buffer.from(base64Data, "base64");
-          images.push(imageBuffer);
-        });
+//         // Insert documentName and get documentId
+//         const [documentResult] = await db.query("INSERT INTO ots_document SET ?", {
+//           documen_name: docName,
+//           testCreationTableId: req.body.testCreationTableId,
+//           subjectId: req.body.subjectId,
+//           sectionId:req.body.sectionId
+//         });
+//         const document_Id = documentResult.insertId;
+    
+//         // Get all images in the order they appear in the HTML
+//         const images = [];
+//         $("img").each(function (i, element) {
+//           const base64Data = $(this)
+//             .attr("src")
+//             .replace(/^data:image\/\w+;base64,/, "");
+//           const imageBuffer = Buffer.from(base64Data, "base64");
+//           images.push(imageBuffer);
+//         });
       
 
-        let j = 0;
-        let Question_id;
-        let question_id=[];
-        for (let i = 0; i < images.length; i++) {
-          if (j == 0) {
-            const questionRecord = {
-              question_img: images[i],
-              testCreationTableId: req.body.testCreationTableId,
-              sectionId: req.body.sectionId,
-              document_Id: document_Id,
-              subjectId: req.body.subjectId,
-            };
-            console.log(j); 
-            Question_id = await insertRecord("questions", questionRecord);
-            question_id.push(Question_id)
-            j++;
-          }
-           else if (j > 0 && j < 5) {
-            const optionRecord = {
-              option_img: images[i],
-              question_id: Question_id,
-            };
-            console.log(j);
-            await insertRecord("options", optionRecord);
-            j++;
-          } else if (j == 5) {
-            const solutionRecord = {
-              solution_img: images[i],
-              question_id: Question_id,
-            };
-            console.log(j);
-            await insertRecord("solution", solutionRecord);
-            j = 0;
-          }
-        }
-        let que_id;
-        let qtypeMappings = {
-          mcq: 1,
-          msq: 2,
-          nsq: 3,
-          'True/False Questions': 4,
-        };
+//         let j = 0;
+//         let Question_id;
+//         let question_id=[];
+//         for (let i = 0; i < images.length; i++) {
+//           if (j == 0) {
+//             const questionRecord = {
+//               question_img: images[i],
+//               testCreationTableId: req.body.testCreationTableId,
+//               sectionId: req.body.sectionId,
+//               document_Id: document_Id,
+//               subjectId: req.body.subjectId,
+//             };
+//             console.log(j); 
+//             Question_id = await insertRecord("questions", questionRecord);
+//             question_id.push(Question_id)
+//             j++;
+//           }
+//            else if (j > 0 && j < 5) {
+//             const optionRecord = {
+//               option_img: images[i],
+//               question_id: Question_id,
+//             };
+//             console.log(j);
+//             await insertRecord("options", optionRecord);
+//             j++;
+//           } else if (j == 5) {
+//             const solutionRecord = {
+//               solution_img: images[i],
+//               question_id: Question_id,
+//             };
+//             console.log(j);
+//             await insertRecord("solution", solutionRecord);
+//             j = 0;
+//           }
+//         }
+//         let que_id;
+//         let qtypeMappings = {
+//           mcq: 1,
+//           msq: 2,
+//           nsq: 3,
+//           'True/False Questions': 4,
+//         };
 
-        for (let i = 0; i < textSections.length; i++) {
-          if (textSections[i].startsWith('[qtype]')) {
-            que_id=question_id[j];
-            j++;
-            const qtypeText = textSections[i].replace('[qtype]', '').trim().toLowerCase();
-            // Save in the qtype table
-            if (qtypeMappings.hasOwnProperty(qtypeText)) {
-              // Save in the qtype table
-              const qtypeRecord = {
-                qtype_text: textSections[i].replace('[qtype]', ''),
-                question_id: que_id,
-                quesionTypeId: qtypeMappings[qtypeText],
-              };
-              await insertRecord('qtype', qtypeRecord);
-            } else {
-              // Handle invalid qtypeText
-              console.error(`Invalid qtype text: ${qtypeText}`);
-              // You can choose to throw an error, skip the record, or handle it in any other way.
-            }
-          } else if (textSections[i].startsWith('[ans]')) {
-            // Save in the answer table
-            const answerRecord = {
-              answer_text: textSections[i].replace('[ans]', ''),
-              question_id: que_id
-            };
-            await insertRecord('answer', answerRecord);
-          } else if (textSections[i].startsWith('[Marks]')) {
-            // Save in the marks table
-            const marksRecord = {
-              marks_text: textSections[i].replace('[Marks]', ''),
-              question_id: que_id
-            };
-            await insertRecord('marks', marksRecord);
-          }  
-          else if (textSections[i].startsWith('[sortid]')) {
-            const sortidRecord = {
-              sortid_text: textSections[i].replace('[sortid]', ''),
-              question_id: que_id
-            };
-            await insertRecord('sortid', sortidRecord);
-          }  
-        }
+//         for (let i = 0; i < textSections.length; i++) {
+//           if (textSections[i].startsWith('[qtype]')) {
+//             que_id=question_id[j];
+//             j++;
+//             const qtypeText = textSections[i].replace('[qtype]', '').trim().toLowerCase();
+//             // Save in the qtype table
+//             if (qtypeMappings.hasOwnProperty(qtypeText)) {
+//               // Save in the qtype table
+//               const qtypeRecord = {
+//                 qtype_text: textSections[i].replace('[qtype]', ''),
+//                 question_id: que_id,
+//                 quesionTypeId: qtypeMappings[qtypeText],
+//               };
+//               await insertRecord('qtype', qtypeRecord);
+//             } else {
+//               // Handle invalid qtypeText
+//               console.error(`Invalid qtype text: ${qtypeText}`);
+//               // You can choose to throw an error, skip the record, or handle it in any other way.
+//             }
+//           } else if (textSections[i].startsWith('[ans]')) {
+//             // Save in the answer table
+//             const answerRecord = {
+//               answer_text: textSections[i].replace('[ans]', ''),
+//               question_id: que_id
+//             };
+//             await insertRecord('answer', answerRecord);
+//           } else if (textSections[i].startsWith('[Marks]')) {
+//             // Save in the marks table
+//             const marksRecord = {
+//               marks_text: textSections[i].replace('[Marks]', ''),
+//               question_id: que_id
+//             };
+//             await insertRecord('marks', marksRecord);
+//           }  
+//           else if (textSections[i].startsWith('[sortid]')) {
+//             const sortidRecord = {
+//               sortid_text: textSections[i].replace('[sortid]', ''),
+//               question_id: que_id
+//             };
+//             await insertRecord('sortid', sortidRecord);
+//           }  
+//         }
 
-        res.send(
-          "Text content and images extracted and saved to the database with the selected topic ID successfully."
-        );
-      } catch (error) {
-        console.error(error);
-        res
-          .status(500)
-          .send("Error extracting content and saving it to the database.");
-      }
-    });
+//         res.send(
+//           "Text content and images extracted and saved to the database with the selected topic ID successfully."
+//         );
+//       } catch (error) {
+//         console.error(error);
+//         res
+//           .status(500)
+//           .send("Error extracting content and saving it to the database.");
+//       }
+//     });
 
 
 
